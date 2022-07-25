d2bc5c7d30f2e86042af057a64536166b34046ca
Jim Sharples <jamie.sharples@gmail.com>
2022-07-25 18:07:28 +1000

    initial

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export {HTMLtoPDFHelper} from './HTMLtoPDFHelper';
+
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import {PatientXMLImporter} from "./PatientXMLImporter";
+import * as dotenv from 'dotenv';
+
+// dotenv.config();
+//
+// console.log(process.env);
+//
+// PatientXMLImporter.getInstance().convertFileToPatient('',process.argv[2],process.argv[2]).then((patient) => {
+//     console.log(patient);
+// })
